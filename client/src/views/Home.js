223b56d8ed3f64350c7b95d7e037ed0937b3062f
adamdune <adam.ho.buss@gmail.com>
2021-01-10 11:22:00 +0800

    fix: use local spacex logo

--- client/src/views/Home.js
+++ client/src/views/Home.js
@@ -1,49 +1,50 @@
-import React, { Fragment } from 'react';
-import { Query } from 'react-apollo';
-import gql from 'graphql-tag';
-import LaunchItem from '../components/LaunchItem';
-import HistoryItem from '../components/HistoryItem';
-import Loading from '../components/Loading';
-import Error from '../components/Error';
-import MissionLegends from '../components/MissionLegends';
-import DateDislacimer from '../components/DateDisclaimer';
+import React, { Fragment } from "react"
+import { Query } from "react-apollo"
+import gql from "graphql-tag"
+import LaunchItem from "../components/LaunchItem"
+import HistoryItem from "../components/HistoryItem"
+import Loading from "../components/Loading"
+import Error from "../components/Error"
+import MissionLegends from "../components/MissionLegends"
+import DateDislacimer from "../components/DateDisclaimer"
+import SpaceXLogo from "../components/SpaceXLogo"
 
 const HOME_QUERY = gql`
-fragment launch on Launch{
+  fragment launch on Launch {
     flight_number
     mission_name
     launch_date_utc
     launch_success
     upcoming
-    rocket{
+    rocket {
       rocket_name
       rocket_id
     }
-    links{
+    links {
       article_link
     }
-}
-
-query HomeQuery{
-  info{
-    summary
   }
-  history_latest{
-    title
-    event_date_utc
-    details
-    links{
-      article
-      wikipedia
+
+  query HomeQuery {
+    info {
+      summary
+    }
+    history_latest {
+      title
+      event_date_utc
+      details
+      links {
+        article
+        wikipedia
+      }
+    }
+    launch_latest {
+      ...launch
+    }
+    launch_next {
+      ...launch
     }
   }
-  launch_latest{
-    ...launch
-  }
-  launch_next{
-    ...launch
-  }
-}
 `
 
 const Home = () => {
@@ -53,42 +54,36 @@ const Home = () => {
         if (loading) return <Loading />
         if (error) return <Error />
 
-        if (data.info){
-        return (
-          <Fragment>
-            <div className="jumbotron bg-dark text-white text-center">
-              <img src="https://www.spacex.com/sites/all/themes/spacex2012/logo.png" alt="SpaceX Logo" className="img-fluid mb-3" />
-              <p className="lead">
-                {data.info.summary}
-              </p>
-            </div>
-            <HistoryItem
-              history={data.history_latest}
-            />
-            <div className="row mb-3">
-              <div className="col-sm-12 col-md-6">
-                <h3 className="ml-1">Latest Launch</h3>
-                <LaunchItem
-                  launch={data.launch_latest}
-                />
+        if (data.info) {
+          return (
+            <Fragment>
+              <div className="jumbotron bg-dark text-white text-center">
+                <div className="mb-4">
+                  <SpaceXLogo width={300} />
+                </div>
+                <p className="lead">{data.info.summary}</p>
               </div>
-              <div className="col-sm-12 col-md-6">
-                <h3 className="ml-1">Upcoming Launch</h3>
-                <LaunchItem
-                  launch={data.launch_next}
-                />
+              <HistoryItem history={data.history_latest} />
+              <div className="row mb-3">
+                <div className="col-sm-12 col-md-6">
+                  <h3 className="ml-1">Latest Launch</h3>
+                  <LaunchItem launch={data.launch_latest} />
+                </div>
+                <div className="col-sm-12 col-md-6">
+                  <h3 className="ml-1">Upcoming Launch</h3>
+                  <LaunchItem launch={data.launch_next} />
+                </div>
+                <div className="col-sm-12">
+                  <MissionLegends />
+                </div>
               </div>
-              <div className="col-sm-12">
-                <MissionLegends />
-              </div>
-            </div>
-            <DateDislacimer />
-          </Fragment>
-        )
-      } else return <Error/>
+              <DateDislacimer />
+            </Fragment>
+          )
+        } else return <Error />
       }}
     </Query>
-  );
+  )
 }
 
-export default Home;
+export default Home
--- client/src/views/Info.js
+++ client/src/views/Info.js
@@ -1,104 +1,121 @@
-import React, { Component, Fragment } from 'react';
-import gql from 'graphql-tag';
-import { Query } from 'react-apollo';
-import { formatNumber } from '../utility';
-import Loading from '../components/Loading';
-import Error from '../components/Error';
-
+import React, { Component, Fragment } from "react"
+import gql from "graphql-tag"
+import { Query } from "react-apollo"
+import { formatNumber } from "../utility"
+import Loading from "../components/Loading"
+import Error from "../components/Error"
+import SpaceXLogo from "../components/SpaceXLogo"
 
 const INFO_QUERY = gql`
-query InfoQuery{
-  info{
-    name
-    founder
-    founded
-    employees
-    ceo
-    cto
-    coo
-    cto_propulsion
-    valuation
-    summary
-    headquarters{
-      address
-      city	
-      state
+  query InfoQuery {
+    info {
+      name
+      founder
+      founded
+      employees
+      ceo
+      cto
+      coo
+      cto_propulsion
+      valuation
+      summary
+      headquarters {
+        address
+        city
+        state
+      }
     }
   }
-}
 `
 
 class Info extends Component {
-    
-    componentDidMount(){
-        document.title = 'Info | SpaceX API View';
-    }
+  componentDidMount() {
+    document.title = "Info | SpaceX API View"
+  }
 
-    render() {
-        return (
-            <Query query={INFO_QUERY}>
-                {({ loading, error, data }) => {
-                    if (loading) return <Loading />
-                    if (error) return <Error />
+  render() {
+    return (
+      <Query query={INFO_QUERY}>
+        {({ loading, error, data }) => {
+          if (loading) return <Loading />
+          if (error) return <Error />
 
-                    if (data.info) {
-                        const info = data.info;
-                        const keyPeopleRender = [
-                            { key: 'CEO', value: info.ceo },
-                            { key: 'CTO', value: info.cto },
-                            { key: 'COO', value: info.coo },
-                            { key: 'CTO Propulsion', value: info.cto_propulsion }
-                        ]
-                        return (
-                            <Fragment>
-                                <div className="jumbotron bg-dark text-light text-center py-4">
-                                    <img src="https://www.spacex.com/sites/all/themes/spacex2012/logo.png" alt="SpaceX Logo" className="img-fluid mb-3" />
-                                    <p className="lead">
-                                        {info.summary}
-                                    </p>
-                                    <hr className="my-4 bg-light" />
-                                    <p className="lead">
-                                        {info.name} was founded by {info.founder} in {info.founded}, it currently has ~{formatNumber(info.employees)} employees and an estimated valuation of ${formatNumber(info.valuation)}.
-                                </p>
-                                </div>
-                                <h3 className="text-center">Key People</h3>
-                                <div className="container">
-                                    <div className="row bg-dark text-white mb-4">
-                                        {
-                                            keyPeopleRender.map((keyPeople, index) =>
-                                                <div className="col-sm-6 col-md-3 pt-2 border-right border-bottom"
-                                                    key={`keyPeople${index}`}>
-                                                    <p className="mb-0">{keyPeople.key}</p>
-                                                    <p><strong>{keyPeople.value}</strong></p>
-                                                </div>
-                                            )
-                                        }
-                                    </div>
-                                </div>
-                                <h3 className="text-center">Location</h3>
-                                <div className="container bg-dark text-center py-3 mb-3">
-                                    <h5 className="text-light"><i className="fas fa-map-marker-alt mr-2"></i> Headquarters</h5>
-                                    <h4 className="text-light">{info.headquarters.address}, {info.headquarters.city}, {info.headquarters.state}</h4>
-                                    <div className="container-fluid">
-                                        <div
-                                            style={{
-                                                overflow: 'hidden',
-                                                position: 'relative'
-                                            }}
-                                        >
-                                            <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d1655.3909253652128!2d-118.3281537!3d33.9210137!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80c2b5dee46db32d%3A0x5589bf4232c10232!2sSpaceX!5e0!3m2!1sen!2smy!4v1551366586581"
-                                                title="Google Map - SpaceX Headquarters" style={{ width: "600px", height: "450px", frameborder: 0, border: 0 }}
-                                                allowfullscreen></iframe>
-                                        </div>
-                                    </div>
-                                </div>
-                            </Fragment>
-                        )
-                    } else return <Error/>
-                }}
-            </Query>
-        );
-    }
+          if (data.info) {
+            const info = data.info
+            const keyPeopleRender = [
+              { key: "CEO", value: info.ceo },
+              { key: "CTO", value: info.cto },
+              { key: "COO", value: info.coo },
+              { key: "CTO Propulsion", value: info.cto_propulsion },
+            ]
+            return (
+              <Fragment>
+                <div className="jumbotron bg-dark text-light text-center py-4">
+                  <div className="mb-4">
+                    <SpaceXLogo width={300} />
+                  </div>
+                  <p className="lead">{info.summary}</p>
+                  <hr className="my-4 bg-light" />
+                  <p className="lead">
+                    {info.name} was founded by {info.founder} in {info.founded},
+                    it currently has ~{formatNumber(info.employees)} employees
+                    and an estimated valuation of $
+                    {formatNumber(info.valuation)}.
+                  </p>
+                </div>
+                <h3 className="text-center">Key People</h3>
+                <div className="container">
+                  <div className="row bg-dark text-white mb-4">
+                    {keyPeopleRender.map((keyPeople, index) => (
+                      <div
+                        className="col-sm-6 col-md-3 pt-2 border-right border-bottom"
+                        key={`keyPeople${index}`}
+                      >
+                        <p className="mb-0">{keyPeople.key}</p>
+                        <p>
+                          <strong>{keyPeople.value}</strong>
+                        </p>
+                      </div>
+                    ))}
+                  </div>
+                </div>
+                <h3 className="text-center">Location</h3>
+                <div className="container bg-dark text-center py-3 mb-3">
+                  <h5 className="text-light">
+                    <i className="fas fa-map-marker-alt mr-2"></i> Headquarters
+                  </h5>
+                  <h4 className="text-light">
+                    {info.headquarters.address}, {info.headquarters.city},{" "}
+                    {info.headquarters.state}
+                  </h4>
+                  <div className="container-fluid">
+                    <div
+                      style={{
+                        overflow: "hidden",
+                        position: "relative",
+                      }}
+                    >
+                      <iframe
+                        src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d1655.3909253652128!2d-118.3281537!3d33.9210137!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80c2b5dee46db32d%3A0x5589bf4232c10232!2sSpaceX!5e0!3m2!1sen!2smy!4v1551366586581"
+                        title="Google Map - SpaceX Headquarters"
+                        style={{
+                          width: "600px",
+                          height: "450px",
+                          frameborder: 0,
+                          border: 0,
+                        }}
+                        allowfullscreen
+                      ></iframe>
+                    </div>
+                  </div>
+                </div>
+              </Fragment>
+            )
+          } else return <Error />
+        }}
+      </Query>
+    )
+  }
 }
 
-export default Info;
+export default Info
